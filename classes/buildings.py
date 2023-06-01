84f55132a270884898c7099ea3fd74f9f73fb465
Justin Turney <info@justinturney.com>
2023-06-01 14:33:12 -0500

    initial commit

---
+++ classes/buildings.py
@@ -0,0 +1,10 @@
+
+
+class Base():
+    def __init__(self, civ: int, epoch: int, level:int, hp: int, size: tuple, position: tuple) -> None:
+        self.civ = civ
+        self.epoch = epoch
+        self.level = level
+        self.hp = hp
+        self.size = size
+        self.position = position
